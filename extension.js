f032ebfe69434a4a1a8c7f78a94fa1d232444453
sunzhaoxiang <sunzhaoxiang@baidu.com>
2021-12-20 21:33:00 +0800

    格式转换

--- extension.js
+++ extension.js
@@ -1,34 +1,59 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const {toCamelCase, formatConversionCheck} = require("./utils/index");
 
 /**
- * @param {vscode.ExtensionContext} context
+ * 插件被激活时触发，所有代码总入口
+ * @param {*} context 插件上下文
  */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-extension-demo" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-extension-demo.helloWorld', function (uri) {
-		// The code you place here will be executed every time your command is executed
-		const showMessage = `vscode插件实现示例：Hello World Demo from vscode-extension-demo! 当前文件(夹)路径是:${uri}`
-		// Display a message box to the user
+const activate = (context) => {
+	console.log('恭喜，您的扩展“vscode-extensions-demo”已被激活！');
+	// Hello Word示例
+	// 命令事件进行注册
+	let disposableOne = vscode.commands.registerCommand('vscode-extension-demo.helloWorld', () => {
+		// 命令执行时触发
+		const showMessage = `vscode插件实现示例：Hello World Demo from vscode-extension-demo!`
 		vscode.window.showInformationMessage(showMessage);
 	});
 
-	context.subscriptions.push(disposable);
+	// 格式转换：snakeCase to camelCase
+	let disposableTwo = vscode.commands.registerCommand(
+		"formatConversion.camelCase",
+		() => {
+		  const activeEditor = vscode.window.activeTextEditor;
+		  if (!activeEditor) {
+			return;
+		  }
+		  const { selection } = activeEditor;
+		  const selected = activeEditor.document.getText(selection);
+		  try {
+			if(formatConversionCheck(selected)) {
+				return vscode.window.showInformationMessage(
+					"亲：这种格式我转换不了呀 ~ "
+				);
+			}
+			if (selected.length > 66) {
+				return vscode.window.showInformationMessage(
+					"亲：太长了我转换不了啦 ~ "
+				);
+			}
+			const humpNamingString = toCamelCase(selected);
+			activeEditor.edit((editBuilder) => {
+			  editBuilder.replace(activeEditor.selection, humpNamingString);
+			});
+		  } catch (error) {
+			console.log("error", error);
+			vscode.window.showInformationMessage("程序错误" + error);
+		  }
+		}
+	);
+
+	context.subscriptions.push(...[disposableOne, disposableTwo]);
 }
 
-// this method is called when your extension is deactivated
-function deactivate() {}
+/**
+ * 插件被释放时触发
+ */
+const deactivate = () => console.log('您的扩展“vscode-plugin-demo”已被释放！');
 
 module.exports = {
 	activate,
---
+++ utils/index.js
@@ -0,0 +1,19 @@
+const toCamelCase = str => {
+    // _\w 表示_和后面的一个字母。(\w)括号扩起来表示子项。g表示所有的
+    var reg = /_(\w)/g;
+    // 形参$0表示正则的整体_(\w)，$1表示正则的第一个子项(\w)
+    return str.replace(reg, ($0, $1) =>  $1.toUpperCase())
+}
+
+const toSnakeCase = str => {
+    return str.replace(/([A-Z])/g, $1 => '_' + $1.toLocaleLowerCase());
+}
+
+const formatConversionCheck = str => str.indexOf('_') === -1;
+
+module.exports = {
+    toCamelCase,
+    formatConversionCheck,
+    toSnakeCase
+};
+  
