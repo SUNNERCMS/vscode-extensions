1efd9ff4cd019352394b042288c0e41fb2b63ee7
zhaoxiang.sun <sunnercms@163.com>
2021-12-29 19:44:35 +0800

    utils

--- utils/index.js
+++ utils/index.js
@@ -1,16 +1,3 @@
-// // snakeCase to camelCase
-// const toCamelCase = str => {
-//     // _\w 表示_和后面的一个字母。(\w)括号扩起来表示子项。g表示所有的
-//     var reg = /_(\w)/g;
-//     // 形参$0表示正则的整体_(\w)，$1表示正则的第一个子项(\w)
-//     return str.replace(reg, ($0, $1) =>  $1.toUpperCase())
-// }
-
-// camelCase to snakeCase
-// const toSnakeCase = str => {
-//     return str.replace(/([A-Z])/g, $1 => '_' + $1.toLowerCase());
-// }
-
 const formatConversionCheck = str => str.indexOf('_') === -1;
 
 const humpNaming = str => {
@@ -50,7 +37,6 @@ const toSnakeCase = (str, formatType) => {
     // _\w 表示_和后面的一个字母。(\w)括号扩起来表示子项。g表示所有的
     const reg1 = /_(\w)/g;
     const reg2 = /-(\w)/g;
-    console.log('ssss----:', formatType)
     switch(formatType) {
         case 'snakeCase':
             if(reg1.test(str)) {
@@ -90,7 +76,6 @@ const toSnakeCase = (str, formatType) => {
 }
 
 module.exports = {
-    // toCamelCase,
     formatConversionCheck,
     toSnakeCase,
     humpNaming,
