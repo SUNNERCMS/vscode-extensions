c4f43429072b8ce778d061e8ca2a0180b6a12549
sunzhaoxiang <sunzhaoxiang@baidu.com>
2021-12-29 21:22:07 +0800

    代码片段

--- utils/Translator.js
+++ utils/Translator.js
@@ -46,6 +46,8 @@ Translator.prototype.generateUrlParams = function generateUrlParams(_params) {
 
 /**
  * 进行翻译
+ * 有道翻译api
+ * https://ai.youdao.com/DOCSIRMA/html/%E8%87%AA%E7%84%B6%E8%AF%AD%E8%A8%80%E7%BF%BB%E8%AF%91/API%E6%96%87%E6%A1%A3/%E6%96%87%E6%9C%AC%E7%BF%BB%E8%AF%91%E6%9C%8D%E5%8A%A1/%E6%96%87%E6%9C%AC%E7%BF%BB%E8%AF%91%E6%9C%8D%E5%8A%A1-API%E6%96%87%E6%A1%A3.html
  */
 Translator.prototype.translate = async function (word) {
   let youdaoHost = "http://openapi.youdao.com/api";
